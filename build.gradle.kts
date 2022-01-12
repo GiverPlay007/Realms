c25f5e781fe122333579f4a7849b340b82240b54
GiverPlay007 <play.giver.007@gmail.com>
2022-01-12 00:07:28 -0300

    Initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+    java
+}
+
+group = "net.focaenterprises"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ src/main/java/net/focaenterprises/Game.java
@@ -0,0 +1,7 @@
+package net.focaenterprises;
+
+public class Game {
+  public static void main(String[] args) {
+    System.out.println("Oi");
+  }
+}
